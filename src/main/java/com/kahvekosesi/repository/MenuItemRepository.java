f8fbe0ac717cd85b6f9d3dfa24d3afca241c9a33
Abdullah Okyay <okyayabdullah2@gmail.com>
2026-05-16 17:26:31 +0300

    add repository package and implement repositories for MenuItem and RestaurantTable

---
+++ src/main/java/com/kahvekosesi/repository/MenuItemRepository.java
@@ -0,0 +1,9 @@
+package com.kahvekosesi.repository;
+
+import com.kahvekosesi.entity.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+}

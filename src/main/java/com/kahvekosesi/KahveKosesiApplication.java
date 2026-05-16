80788aa20ab69902acb64510fe4ec20e524e61e8
Abdullah Okyay <okyayabdullah2@gmail.com>
2026-05-16 16:41:04 +0300

    initial repository setup for Kahve Kosesi

---
+++ src/main/java/com/kahvekosesi/KahveKosesiApplication.java
@@ -0,0 +1,13 @@
+package com.kahvekosesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahveKosesiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KahveKosesiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kahvekosesi/KahveKosesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kahvekosesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahveKosesiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

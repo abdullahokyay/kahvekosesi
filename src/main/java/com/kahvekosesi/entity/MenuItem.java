e01eeebdf4d431b3369b6565516e5f6288b17247
Abdullah Okyay <okyayabdullah2@gmail.com>
2026-05-16 17:07:39 +0300

    add MenuItem and RestaurantTable classes under entity package

---
+++ src/main/java/com/kahvekosesi/entity/MenuItem.java
@@ -0,0 +1,24 @@
+package com.kahvekosesi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double price;
+    private String category;
+}
